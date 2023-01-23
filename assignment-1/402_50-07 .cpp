a455e59bad689145ce6dcec112c300bbb3acd304
Nurul Karim <122337273+nurul898@users.noreply.github.com>
2023-01-24 02:23:34 +0600

    Add files via upload

--- assignment-1/402_50-07 .cpp
+++ assignment-1/402_50-07 .cpp
@@ -1,19 +1,24 @@
 #include <iostream>
-#include <cctype>
 using namespace std;
-int main(){
- int n,i=1,sum=0;
- cout << "Enter a number: ";
+int main() {
+ int n, t1 = 0, t2 = 1, nextTerm = 0;
+ cout << "Enter the number of terms: ";
  cin >> n;
- while(i<n){
- if(n%i==0)
- sum=sum+i;
- i++;
-}
-if(sum==n)
- cout << i << " is a perfect number\n";
-else
- cout << i << " is not a perfect number\n";
- system("pause");
-return 0;
+ cout << "Fibonacci Series: ";
+ for (int i = 1; i <= n; ++i) {
+ // Prints the first two terms.
+ if(i == 1) {
+ cout << t1 << ", ";
+ continue;
+ }
+ if(i == 2) {
+ cout << t2 << ", ";
+ continue;
+ }
+ nextTerm = t1 + t2;
+ t1 = t2;
+ t2 = nextTerm;
+ cout << nextTerm << ", ";
+ }
+ return 0;
 }
